837c5cbc565a60dfdbd94c44a0fe4d2bd4a6f6b6
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2024-05-05 11:38:22 +0300

    deployment changes made

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
@@ -13,6 +14,7 @@ import MONGODB_CONNECTION from "./database/mongodb.js";
 
 // const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
@@ -21,6 +23,12 @@ app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.get("/test", (req, res) => {
   res.status(200).json("local server running here😊");
