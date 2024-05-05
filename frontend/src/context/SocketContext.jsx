8bec4462020459fe1821190f7650576ab48c412e
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2024-05-05 12:12:15 +0300

    socket route updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://mern-chat-app-practice-2.onrender.com", {
         query: {
           userId: authUser._id,
         },
@@ -43,48 +43,3 @@ export const SocketContextProvider = ({ children }) => {
   );
 };
 
-// import { createContext, useState, useEffect, useContext } from "react";
-// import { useAuthContext } from "./AuthContext";
-// import io from "socket.io-client";
-
-// const SocketContext = createContext();
-
-// export const useSocketContext = () => {
-//   return useContext(SocketContext);
-// };
-
-// export const SocketContextProvider = ({ children }) => {
-//   const [socket, setSocket] = useState(null);
-//   const [onlineUsers, setOnlineUsers] = useState([]);
-//   const { authUser } = useAuthContext();
-
-//   useEffect(() => {
-//     if (authUser) {
-//       const socket = io("http://localhost:8000", {
-//         query: {
-//           userId: authUser._id,
-//         },
-//       });
-
-//       setSocket(socket);
-
-//       // socket.on() is used to listen to the events. can be used both on client and server side
-//       socket.on("getOnlineUsers", (users) => {
-//         setOnlineUsers(users);
-//       });
-
-//       return () => socket.close();
-//     } else {
-//       if (socket) {
-//         socket.close();
-//         setSocket(null);
-//       }
-//     }
-//   }, [authUser]);
-
-//   return (
-//     <SocketContext.Provider value={{ socket, onlineUsers }}>
-//       {children}
-//     </SocketContext.Provider>
-//   );
-// };
